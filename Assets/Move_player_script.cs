c9b08526e805151363eeb284afcab2809cd25204
tantuan003 <tantuan0162@gmail.com>
2024-12-13 11:17:47 +0700

    add gun and shoot event

---
+++ Assets/Move_player_script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_player_script : MonoBehaviour
+{
+    public Joystick joystick;
+    public float speed = 10;
+    private Vector3 originalScale;
+    void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = joystick.Horizontal;
+        float vertical = joystick.Vertical;
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+        if (direction.x > 0.1f)
+        {
+            transform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z); 
+        }
+        else if (direction.x < -0.1f)
+        {     
+        
+            transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z);
+        }
+    }
+}
---
+++ Assets/Script/Gun_Controller_script.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gun_Controller_script : MonoBehaviour
+{
+    public Transform gunTransform;
+    public Joystick joystick;
+    private Vector3 originalScale;
+    public GameObject BulletPrefab;
+    public float bulletspeed = 10;
+    public Button firebutton;
+    private void Start()
+    {
+        originalScale = transform.localScale;
+        firebutton.onClick.AddListener(shoot);
+    }
+
+    void Update()
+    {
+        // Lấy giá trị từ Joystick
+        float horizontal = joystick.Horizontal;
+        float vertical = joystick.Vertical;
+
+        // Tính toán góc quay
+        Vector2 direction = new Vector2(horizontal, vertical);
+
+        // Xoay súng theo hướng joystick
+        if (direction.magnitude > 0.1f)
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            
+            gunTransform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+
+        // Kiểm tra hướng di chuyển của nhân vật (hướng Joystick)
+        if (direction.x > 0.1f)
+        {
+            transform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z);
+        }
+        else if (direction.x < -0.1f)
+        {
+
+            transform.localScale = new Vector3(-originalScale.x, -originalScale.y, originalScale.z);
+        }
+    }
+    void shoot()
+    {
+        GameObject bullet = Instantiate(BulletPrefab, gunTransform.position, gunTransform.rotation);
+        Vector2 shootingDirection = gunTransform.right;
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.velocity = shootingDirection * bulletspeed; // Gắn tốc độ cho viên đạn
+        }
+        Destroy(bullet, 7);
+    }
+}
