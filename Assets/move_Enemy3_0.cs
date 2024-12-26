551bc75819debb5946b9d12a116ef9ff834c164f
tantuan003 <tantuan0162@gmail.com>
2024-12-26 19:56:54 +0700

    update

---
+++ Assets/move_Enemy3_0.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_Enemy3_0 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb != null)
+        {
+            Vector2 moveDirection = Vector2.right;
+            rb.velocity = moveDirection * speed; // Thiết lập vận tốc
+        }
+    }
+}
---
+++ Assets/spawn_trap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_trap : MonoBehaviour
+{
+    public Transform finalspawn; 
+    public GameObject prefabEnemy; 
+    private bool hasSpawned = false; 
+
+    void Start()
+    {
+        hasSpawned = false;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !hasSpawned)
+        {
+            hasSpawned = true; // Đánh dấu đã spawn
+            GameObject enemy = Instantiate(prefabEnemy, finalspawn.position, Quaternion.Euler(0, 0, 0));
+
+            // Đảm bảo không bị gắn parent
+            enemy.transform.parent = null;          
+        }
+    }
+}
